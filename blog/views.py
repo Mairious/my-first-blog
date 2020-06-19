8f3e1ee76596ddcdd033168553adccaf5f5dda27
Your Name <you@example.com>
2020-06-18 20:13:31 -0400

    Changed view settings so that it was fixed.

--- blog/views.py
+++ blog/views.py
@@ -9,12 +9,10 @@ from django.contrib.auth.decorators import login_required
 
 
 
-@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
