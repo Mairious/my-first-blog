471db035becd046f09dd042fb8835c79e85fb434
Your Name <you@example.com>
2020-06-19 19:15:39 -0400

    deleted it again

--- mysite/settings.py
+++ mysite/settings.py
@@ -63,7 +63,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'pages.pages_context.show_pages_menu',
             ],
         },
     },
