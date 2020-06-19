34638963d35789444cd47ab4d6b5b2534559ff13
Your Name <you@example.com>
2020-06-19 16:53:42 -0400

    Nav bar four

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,8 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('our/', views.our_mission, name='our_mission'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.about, name='contact'),
 
 ]
